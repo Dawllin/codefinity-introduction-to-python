db1b2f7df66ceee8fe936c76720ab9a2b14c04f4
Dawllin <>
2025-11-15 11:39:43 +0000

    Combining Conditions

--- conditional_statements/combining_conditions/main.py
+++ conditional_statements/combining_conditions/main.py
@@ -1,8 +1,6 @@
 # The item's discount and stock status have been defined
 discounted = False
 lowStock = True
-#Create a boolean variable promotion that is True 
-#if the item is not discounted and sufficiently stocked.
 movingProduct = discounted or lowStock
 promotion = not lowStock
 print ("Is the item eligible for promotion?", promotion)
